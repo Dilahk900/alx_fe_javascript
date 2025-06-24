61480e1a0f720f1e86181c4d983faafd03314d91
Dilahk900 <khalid.idrissif@gmail.com>
2025-06-25 00:00:36 +0200

    0. Building a Dynamic Content Generator with Advanced DOM Manipulation

---
+++ script.js
@@ -0,0 +1,39 @@
+let quotes = [];
+
+const quote_text = document.getElementById('newQuoteText');
+const quote_category = document.getElementById('newQuoteCategory');
+
+function createAddQuoteForm(){
+  
+  const text = quote_text.value;
+  const category = quote_category.value;
+  quotes.push({quote_text: text , quote_category: category});
+
+  showRandomQuote ();
+}
+
+document.getElementById('newQuote').addEventListener(('click') , ()=>{
+  let quoteForm = document.getElementById('quoteDisplay');
+  let html = '';
+  quotes.forEach((value)=>{
+    html += `<div>${value.quote_text}</div><div>${value.quote_category}</div>`;
+  });
+  quoteForm.innerHTML = html;
+});
+
+
+function showRandomQuote (){
+  let quoteForm = document.querySelector('.quoteForm');
+  /*let html = '';
+  quotes.forEach((value)=>{
+    html += `<div>${value.quote_text}</div><div>${value.quote_category}</div>`;
+  });
+  quoteForm.innerHTML = html;*/
+
+  let randomQuoteIndex = Math.floor((Math.random()*quotes.length));
+
+  quoteForm.innerHTML = `<div>${quotes[randomQuoteIndex].quote_text}</div>
+      <div>${quotes[randomQuoteIndex].quote_category}</div>`;
+
+}
+
